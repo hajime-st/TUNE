8ead86632c3446e039da5de9b88d181cecf7b1f8
hajime-st <bgv88880@gmail.com>
2022-11-18 15:32:51 +0900

    test

--- app/controllers/themes_controller.rb
+++ app/controllers/themes_controller.rb
@@ -1,6 +1,6 @@
 class ThemesController < ApplicationController
   require 'rspotify'
-  RSpotify.authenticate(ENV['SPOTIFY_CLIENT_ID'], ENV['SPOTIFY_SECRET_ID'])
+  RSpotify.authenticate(ENV['SPOTIFY_CLIENT_ID_heroku'], ENV['SPOTIFY_SECRET_ID_heroku'])
 
   def index
     @themes = Theme.all.includes(:user).order(created_at: :desc)
