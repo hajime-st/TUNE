46810f23e86197787112fa270f850a2323fa04fd
hajime-st <bgv88880@gmail.com>
2022-11-22 14:03:36 +0900

    Fix user follow function

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,22 +1,14 @@
 class RelationshipsController < ApplicationController
   # フォローするとき
   def create
-    current_user.follow(params[:user_id])
-    redirect_to request.referer
+    user = User.find(params[:followed_id])
+    current_user.follow(user)
+    redirect_to user
   end
   # フォロー外すとき
   def destroy
-    current_user.unfollow(params[:user_id])
-    redirect_to request.referer  
-  end
-  # フォロー一覧
-  def followings
-    user = User.find(params[:user_id])
-    @users = user.followings
-  end
-  # フォロワー一覧
-  def followers
-    user = User.find(params[:user_id])
-    @users = user.followers
+    user = Relationship.find(params[:id]).followed
+    current_user.unfollow(user)
+    redirect_to user
   end
 end
