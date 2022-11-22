2db9e5b2dbc7cb18a4469e7338261374ceebc537
hajime-st <bgv88880@gmail.com>
2022-11-22 12:07:20 +0900

    Add user profile show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
     @user = User.new
   end
@@ -11,10 +18,44 @@ class UsersController < ApplicationController
       render :new
     end
   end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path
+    else
+      render :edit
+    end
+  end
 
   private
 
   def user_params
     params.require(:user).permit(:email, :password, :password_confirmation, :name)
   end
+
+  # def set_user
+  #   @user = User.find(current_user.id)
+  # end
+
+  # ログイン済みユーザーかどうか確認
+  def logged_in_user
+    unless logged_in?
+      redirect_to login_url
+    end
+  end
+
+  # 正しいユーザーかどうか確認
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless @user == current_user
+  end
+
+  # def user_params
+  #   params.require(:user).permit(:email, :name)
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,11 @@ Rails.application.routes.draw do
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
 
-  resources :users, only: %i[new create] do
+  resources :users do
     member do
       get :following, :followers
     end
   end
-  resource :profile, only: %i[show edit update]
   resources :relationships, only: %i[create destroy]
   resources :themes do
     resources :answers, only: %i[create update destroy]
