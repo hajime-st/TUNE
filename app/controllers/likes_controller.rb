268a89e2a638ac35b4453881842196cfb1700853
hajime-st <bgv88880@gmail.com>
2022-11-26 17:34:35 +0900

    update like button(ajax)

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,13 +1,13 @@
 class LikesController < ApplicationController
   def create
-    answer = Answer.find(params[:answer_id])
-    current_user.like(answer)
-    redirect_to answer.theme
+    @answer = Answer.find(params[:answer_id])
+    current_user.like(@answer)
+    render turbo_stream: turbo_stream.replace('like', partial: 'themes/unlike', locals: { answer: @answer } )
   end
   
   def destroy
-    answer = current_user.likes.find(params[:id]).answer
-    current_user.unlike(answer)
-    redirect_to answer.theme
+    @answer = current_user.likes.find(params[:id]).answer
+    current_user.unlike(@answer)
+    render turbo_stream: turbo_stream.replace('unlike', partial: 'themes/like', locals: { answer: @answer } )
   end
 end
