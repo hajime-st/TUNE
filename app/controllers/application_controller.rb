450fe0596d83f6d1042f78e3c4d30c76e24bae5e
hajime-st <bgv88880@gmail.com>
2022-12-01 18:46:14 +0900

    Add tailwind and daisyui

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
 class ApplicationController < ActionController::Base
   require 'rspotify'
-  RSpotify.authenticate(ENV['SPOTIFY_CLIENT_ID_fly'], ENV['SPOTIFY_SECRET_ID_fly'])
+  RSpotify.authenticate(ENV['SPOTIFY_CLIENT_ID_heroku'], ENV['SPOTIFY_SECRET_ID_heroku'])
 end
