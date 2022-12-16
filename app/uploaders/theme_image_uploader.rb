dd37c4aec0c0f0e9aa31728139e49d776c0c32f2
hajime-st <bgv88880@gmail.com>
2022-12-16 16:03:45 +0900

    Modify picture extention

--- app/uploaders/theme_image_uploader.rb
+++ app/uploaders/theme_image_uploader.rb
@@ -24,7 +24,7 @@ class ThemeImageUploader < CarrierWave::Uploader::Base
   def default_url(*args)
   #   # For Rails 3.1+ asset pipeline compatibility:
   #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-    default_image = ["default_image.png", "default_image2.jpeg"]
+    default_image = ["default_image.png", "default_image2.jpg"]
     if self.model.id % 2 == 0
       default_image[0]
     else
