54adf2112605c1b9b36caf47ba2c84a62f4a194f
hajime-st <bgv88880@gmail.com>
2022-12-26 14:14:06 +0900

    Add protect_from_forgery

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
   require 'rspotify'
   RSpotify.authenticate(ENV['SPOTIFY_CLIENT_ID_heroku'], ENV['SPOTIFY_SECRET_ID_heroku'])
+  protect_from_forgery
 end
