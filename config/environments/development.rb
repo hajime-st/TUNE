1f1eae3e72bd7a01f603658924bbb184fb089556
hajime-st <bgv88880@gmail.com>
2023-01-03 21:50:25 +0900

    Fix:メーラーの設定

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -71,7 +71,7 @@ Rails.application.configure do
   # Mailer settings
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :letter_opener_web
-  # config.action_mailer.default_url_options = Settings.default_url_options.to_h
+  config.action_mailer.default_url_options = Settings.default_url_options.to_h
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
