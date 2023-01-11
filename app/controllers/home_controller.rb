242f55152c8e4eac9702a00481c655ab504f3d59
hajime-st <bgv88880@gmail.com>
2023-01-11 10:06:25 +0900

    Add:利用規約、プライバシーポリシー、問い合わせフォームの追加

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,4 +3,7 @@ class HomeController < ApplicationController
     @themes = Theme.all.includes(:user).order(created_at: :desc)
     @recent_themes = Theme.all.includes(:user).order(created_at: :desc).first(8)
   end
+
+  def terms; end
+  def privacy; end
 end
